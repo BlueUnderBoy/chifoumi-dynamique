a22d5fd110b1ebfa86e1f1b2934016f4ebe2d78b
Jalen Young <120889301+BlueUnderBoy@users.noreply.github.com>
2024-09-11 07:13:15 +0000

    met a jour

--- app.rb
+++ app.rb
@@ -5,10 +5,10 @@ get("/") do
   erb(:homepage)
 end
 
-get("/dice/dynamic/:number_of_dice/:how_many_sides") do
-  @num_dice = params.fetch("number_of_dice").to_i
+get("/dynamic/:number_of_dice/:how_many_sides") do
+  @num_dice = params[:number_of_dice].to_i
 
-  @sides = params.fetch("how_many_sides").to_i
+  @sides = params[:how_many_sides].to_i
 
   @rolls = []
 
