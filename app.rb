b62b02265e6d05f385afdcef50cf5c51287d7007
Jalen Young <120889301+BlueUnderBoy@users.noreply.github.com>
2024-09-11 11:08:25 +0000

    corriger

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ get("/dice/[RANDOM_DICE]/[RANDOM_SIDES]") do
 
     @rolls.push(die)
   end
-  erb(:random)
+  redirect to("/dynamic/#{@random_dice}/#{@random_sides}")
 end
 
 get("/dynamic/:number_of_dice/:how_many_sides") do
